8e26e785cd462d643a91b487cc0d92b055052ea2
Daffa Hariksan <dharikhsan@gmail.com>
2025-12-22 21:51:10 +0800

    update terakhir

--- mynetworth/components/Calculator.js
+++
@@ -1,9 +0,0 @@
-export default function Calculator({ a, b }) {
-  if (b === 0) return <span>Error: Bagi 0</span>;
-  return (
-    <div>
-      <h1>Hasil: {a + b}</h1>
-      <p data-testid="pembagian">{a / b}</p>
-    </div>
-  );
-}
